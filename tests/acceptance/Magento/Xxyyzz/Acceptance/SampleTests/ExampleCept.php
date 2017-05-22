b6dc9478139d1d1976efea25b4ede6e263b7f519
Ji Lu <jilu1@magento.com>
2017-05-22 14:18:59 -0500

    MTA-4213: splitting tooling-demo project into framework and test packages; created new framework package.

--- tests/acceptance/Magento/Xxyyzz/Acceptance/SampleTests/ExampleCept.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Magento\Xxyyzz\Acceptance\SampleTests;
-
-// @group skip
-$I = new \Magento\Xxyyzz\AcceptanceTester(\Codeception\Scenario::$scenario);
-$I->wantTo('perform actions and see result');
--- tests/acceptance/Magento/Xxyyzz/Acceptance/SampleTests/TestStepObjectCept.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Magento\Xxyyzz\Acceptance\SampleTests;
-
-// @group skip
-$I = new \Magento\Xxyyzz\Step\Backend\AdminStep(\Codeception\Scenario::$scenario);
-$I->wantTo('demo the usage of StepObject in Cept');
-$I->loginAsAdmin();
