34f827da54e406bc65dcb6a9ab12cef07aa1564a
Ian Meron <imeron@magento.com>
2017-08-11 15:54:22 -0500

    MQE:171 Update xml schema and references for data operations

--- src/Magento/AcceptanceTestFramework/DataGenerator/Managers/JsonDefinitionManager.php
+++ src/Magento/AcceptanceTestFramework/DataGenerator/Managers/JsonDefinitionManager.php
@@ -12,6 +12,27 @@ class JsonDefinitionManager
     private static $jsonDefinitionManager;
     private $jsonDefinitions;
 
+    const ENTITY_OPERATION_ROOT_TAG = 'operation';
+    const ENTITY_OPERATION_TYPE = 'type';
+    const ENTITY_OPERATION_DATA_TYPE = 'dataType';
+    const ENTITY_OPERATION_URL = 'url';
+    const ENTITY_OPERATION_METHOD = 'method';
+    const ENTITY_OPERATION_AUTH = 'auth';
+    const ENTITY_OPERATION_HEADER = 'header';
+    const ENTITY_OPERATION_HEADER_PARAM = 'param';
+    const ENTITY_OPERATION_HEADER_VALUE = 'value';
+    const ENTITY_OPERATION_URL_PARAM = 'param';
+    const ENTITY_OPERATION_URL_PARAM_TYPE = 'type';
+    const ENTITY_OPERATION_URL_PARAM_KEY = 'key';
+    const ENTITY_OPERATION_URL_PARAM_VALUE = 'value';
+    const ENTITY_OPERATION_ENTRY = 'entry';
+    const ENTITY_OPERATION_ENTRY_KEY = 'key';
+    const ENTITY_OPERATION_ENTRY_VALUE = 'value';
+    const ENTITY_OPERATION_ARRAY = 'array';
+    const ENTITY_OPERATION_ARRAY_KEY = 'key';
+    const ENTITY_OPERATION_ARRAY_VALUE = 'value';
+
+
     public static function getInstance()
     {
         if (!self::$jsonDefinitionManager) {
@@ -30,37 +51,49 @@ class JsonDefinitionManager
     {
         $objectManager = ObjectManagerFactory::getObjectManager();
         $metadataParser = $objectManager->create(MetadataParser::class);
-        foreach ($metadataParser->readMetadata()['metadata'] as $jsonDefName => $jsonDefArray) {
-            $operation = $jsonDefArray['operation'];
-            $dataType = $jsonDefArray['dataType'];
-            $url = $jsonDefArray['url'] ?? null;
-            $method = $jsonDefArray['method'] ?? null;
-            $auth = $jsonDefArray['auth'] ?? null;
+        foreach ($metadataParser->readMetadata()[JsonDefinitionManager::ENTITY_OPERATION_ROOT_TAG] as
+                 $jsonDefName => $jsonDefArray) {
+            $operation = $jsonDefArray[JsonDefinitionManager::ENTITY_OPERATION_TYPE];
+            $dataType = $jsonDefArray[JsonDefinitionManager::ENTITY_OPERATION_DATA_TYPE];
+            $url = $jsonDefArray[JsonDefinitionManager::ENTITY_OPERATION_URL] ?? null;
+            $method = $jsonDefArray[JsonDefinitionManager::ENTITY_OPERATION_METHOD] ?? null;
+            $auth = $jsonDefArray[JsonDefinitionManager::ENTITY_OPERATION_AUTH] ?? null;
             $headers = [];
             $params = [];
             $jsonMetadata = [];
 
-            if (array_key_exists('header', $jsonDefArray)) {
-                foreach ($jsonDefArray['header'] as $headerEntry) {
-                    $headers[] = $headerEntry['param'] . ': ' . $headerEntry['value'];
+            if (array_key_exists(JsonDefinitionManager::ENTITY_OPERATION_HEADER, $jsonDefArray)) {
+                foreach ($jsonDefArray[JsonDefinitionManager::ENTITY_OPERATION_HEADER] as $headerEntry) {
+                    $headers[] = $headerEntry[JsonDefinitionManager::ENTITY_OPERATION_HEADER_PARAM] . ': ' .
+                        $headerEntry[JsonDefinitionManager::ENTITY_OPERATION_HEADER_VALUE];
                 }
             }
 
-            if (array_key_exists('param', $jsonDefArray)) {
-                foreach ($jsonDefArray['param'] as $paramEntry) {
-                    $params[$paramEntry['type']][$paramEntry['key']] = $paramEntry['value'];
+            if (array_key_exists(JsonDefinitionManager::ENTITY_OPERATION_URL_PARAM, $jsonDefArray)) {
+                foreach ($jsonDefArray[JsonDefinitionManager::ENTITY_OPERATION_URL_PARAM] as $paramEntry) {
+                    $params[$paramEntry[JsonDefinitionManager::ENTITY_OPERATION_URL_PARAM_TYPE]]
+                    [$paramEntry[JsonDefinitionManager::ENTITY_OPERATION_URL_PARAM_KEY]] =
+                        $paramEntry[JsonDefinitionManager::ENTITY_OPERATION_URL_PARAM_VALUE];
                 }
             }
 
-            if (array_key_exists('entry', $jsonDefArray)) {
-                foreach ($jsonDefArray['entry'] as $jsonEntryType) {
-                    $jsonMetadata[] = new JsonElement($jsonEntryType['key'], $jsonEntryType['value'], 'entry');
+            if (array_key_exists(JsonDefinitionManager::ENTITY_OPERATION_ENTRY, $jsonDefArray)) {
+                foreach ($jsonDefArray[JsonDefinitionManager::ENTITY_OPERATION_ENTRY] as $jsonEntryType) {
+                    $jsonMetadata[] = new JsonElement(
+                        $jsonEntryType[JsonDefinitionManager::ENTITY_OPERATION_ENTRY_KEY],
+                        $jsonEntryType[JsonDefinitionManager::ENTITY_OPERATION_ENTRY_VALUE],
+                        JsonDefinitionManager::ENTITY_OPERATION_ENTRY
+                    );
                 }
             }
 
-            if (array_key_exists('array', $jsonDefArray)) {
-                foreach ($jsonDefArray['array'] as $jsonEntryType) {
-                    $jsonMetadata[] = new JsonElement($jsonEntryType['key'], $jsonEntryType['value'], 'array');
+            if (array_key_exists(JsonDefinitionManager::ENTITY_OPERATION_ARRAY, $jsonDefArray)) {
+                foreach ($jsonDefArray[JsonDefinitionManager::ENTITY_OPERATION_ARRAY] as $jsonEntryType) {
+                    $jsonMetadata[] = new JsonElement(
+                        $jsonEntryType[JsonDefinitionManager::ENTITY_OPERATION_ARRAY_KEY],
+                        $jsonEntryType[JsonDefinitionManager::ENTITY_OPERATION_ARRAY_VALUE],
+                        JsonDefinitionManager::ENTITY_OPERATION_ARRAY
+                    );
                 }
             }
 
