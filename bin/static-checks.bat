3b84770f4fb4236950bec27062eca598c84ebcda
imeron2433 <imeron@magento.com>
2017-12-07 23:07:31 +0200

    MQE-386: MFTF Compatability with Windows Machine
    - changes to test suite in order to account for os pathing in
    verification tests
    - add .gitattributes to the mftf

---
+++ bin/static-checks.bat
@@ -0,0 +1,20 @@
+:: Copyright © Magento, Inc. All rights reserved.
+:: See COPYING.txt for license details.
+
+@echo off
+@echo ===============================PHP CODE SNIFFER REPORT===============================
+call vendor\bin\phpcs .\src --standard=.\dev\tests\static\Magento
+call vendor\bin\phpcs .\dev\tests\unit --standard=.\dev\tests\static\Magento
+call vendor\bin\phpcs .\dev\tests\verification --standard=.\dev\tests\static\Magento --ignore=dev\tests\verification\_generated
+
+@echo ===============================COPY PASTE DETECTOR REPORT===============================
+call vendor\bin\phpcpd .\src
+
+:: Uncomment lines as part of MQE-590
+:: @echo "===============================PHP MESS DETECTOR REPORT===============================
+:: vendor\bin\phpmd .\src text \dev\tests\static\Magento\CodeMessDetector\ruleset.xml --exclude _generated
+
+@echo ===============================MAGENTO COPYRIGHT REPORT===============================
+echo msgbox "INFO:Copyright check currently not run as part of .bat implementation" > "%temp%\popup.vbs"
+wscript.exe "%temp%\popup.vbs"
+::bin\copyright-check
