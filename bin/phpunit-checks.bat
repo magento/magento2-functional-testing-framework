191ac9e8255589817edf2c114a0e2bf67867f3bd
Tom Reece <treece@magento.com>
2017-12-13 10:55:22 -0600

    MQE-601: PHPUnit Integration for MFTF

--- bin/phpunit-checks.bat
+++ bin/phpunit-checks.bat
@@ -3,13 +3,15 @@
 
 :: REMEMBER TO UPDATE THE BASH FILE
 
+SET UNIT_COVERAGE_THRESHOLD=20
+
 @echo ===============================UNIT TESTS===============================
 @echo off
 call vendor\bin\phpunit --configuration dev\tests\phpunit.xml --testsuite unit --coverage-clover clover.xml
 
 @echo ===========================UNIT TEST COVERAGE===========================
 @echo off
-call vendor\bin\coverage-check clover.xml 20
+call vendor\bin\coverage-check clover.xml %UNIT_COVERAGE_THRESHOLD%
 
 @echo ===========================VERIFICATION TESTS===========================
 @echo off
