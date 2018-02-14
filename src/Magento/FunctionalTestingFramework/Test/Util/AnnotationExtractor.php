aa3aa8eb9ad38fbd24c3af6055f268da803547f0
Tom Erskine <terskine@magento.com>
2018-02-14 16:26:49 -0600

    MQE-561: [ALLURE] Add an enum to the "Severity" annotation xml schema.
    - Added enum of Magento severity values to mergedtestschema.xsd
    - Added conditional to AnnotationExtractor.php to check if anotation is "Severity"
    - Added method transformAllureSeveritytoMagento to AnnotationExtractor.php to transform Magento severity to Allure severity

--- src/Magento/FunctionalTestingFramework/Test/Util/AnnotationExtractor.php
+++ src/Magento/FunctionalTestingFramework/Test/Util/AnnotationExtractor.php
@@ -12,6 +12,13 @@ namespace Magento\FunctionalTestingFramework\Test\Util;
 class AnnotationExtractor extends BaseObjectExtractor
 {
     const ANNOTATION_VALUE = 'value';
+    const MAGENTO_TO_ALLURE_SEVERITY_MAP = [
+        "BLOCKER" => "BLOCKER",
+        "CRITICAL" => "MAJOR",
+        "MAJOR" => "NORMAL",
+        "AVERAGE" => "MINOR",
+        "MINOR" => "TRIVIAL"
+    ];
 
     /**
      * AnnotationExtractor constructor.
@@ -30,19 +37,44 @@ class AnnotationExtractor extends BaseObjectExtractor
      */
     public function extractAnnotations($testAnnotations)
     {
+
         $annotationObjects = [];
         $annotations = $this->stripDescriptorTags($testAnnotations, self::NODE_NAME);
 
         // parse the Test annotations
+
         foreach ($annotations as $annotationKey => $annotationData) {
             $annotationValues = [];
+            // Only transform severity annotation
+            if ($annotationKey == "Severity") {
+                $annotationObjects[$annotationKey] = $this->transformAllureSeverityToMagento($annotationData);
+                continue;
+            }
+
             foreach ($annotationData as $annotationValue) {
                 $annotationValues[] = $annotationValue[self::ANNOTATION_VALUE];
             }
-
             $annotationObjects[$annotationKey] = $annotationValues;
         }
 
         return $annotationObjects;
+    }
+
+    /**
+     * This method transforms Magento severity values from Severity annotation
+     * Returns Allure annotation value
+     *
+     * @param array $annotationData
+     * @return array
+     */
+    public function transformAllureSeverityToMagento($annotationData)
+    {
+        $annotationValue = strtoupper($annotationData[0]);
+        //Mapping Magento severity to Allure Severity
+        //Attempts to resolve annotationValue reference against MAGENTO_TO_ALLURE_SEVERITY_MAP -
+        // if not found returns without modification
+        $allureAnnotation[] = self::MAGENTO_TO_ALLURE_SEVERITY_MAP[$annotationValue] ?? $annotationValue;
+
+        return $allureAnnotation;
     }
 }
