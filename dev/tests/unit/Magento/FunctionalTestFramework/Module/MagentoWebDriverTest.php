33e95b66fa6758ab2ea30a91d95d56d5118cb0de
Vasyl Malanka <vsl.malanka@gmail.com>
2018-03-24 17:38:55 +0200

    magento/magento2-functional-testing-framework#54: Set a global value for timeouts that's used across all wait related actions
    - Added Unit test for getDefaultWaitTimeout method

---
+++ dev/tests/unit/Magento/FunctionalTestFramework/Module/MagentoWebDriverTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace tests\unit\Magento\FunctionalTestFramework\Module;
+
+use PHPUnit\Framework\TestCase;
+use Magento\FunctionalTestingFramework\Module\MagentoWebDriver;
+
+class MagentoWebDriverTest extends TestCase
+{
+    /**
+     * Method should return either .env file value or constant value
+     */
+    public function testGetDefaultWaitTimeout()
+    {
+        $this->assertEquals(MagentoWebDriver::getDefaultWaitTimeout(), MagentoWebDriver::DEFAULT_WAIT_TIMEOUT);
+
+        $envFile = new \Dotenv\Dotenv(__DIR__ . '/../../../../../../');
+        $envFile->load();
+
+        $this->assertEquals(MagentoWebDriver::getDefaultWaitTimeout(), 20);
+    }
+}
