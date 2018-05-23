5b51dd624c8b6e8105790b4f84c6aee5a1aa7fc0
aljcalandra <calandra.aj@gmail.com>
2018-05-23 11:54:44 -0500

    MQE-943:  Inconsistency in *Page.xml files declaration for "module" attribute.
    - Moved Module Extractor method
    - Extended Page Filesystem and Dom
    - Added warning for incorrectly named page modules

---
+++ src/Magento/FunctionalTestingFramework/Page/Config/Reader/Filesystem.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\FunctionalTestingFramework\Page\Config\Reader;
+
+use Magento\FunctionalTestingFramework\Exceptions\Collector\ExceptionCollector;
+use Magento\FunctionalTestingFramework\Util\Iterator\File;
+
+class Filesystem extends \Magento\FunctionalTestingFramework\Config\Reader\Filesystem
+{
+    /**
+     * Method to redirect file name passing into Dom class
+     *
+     * @param File $fileList
+     * @return array
+     * @throws \Exception
+     */
+    public function readFiles($fileList)
+    {
+        $exceptionCollector = new ExceptionCollector();
+        $errors = [];
+        /** @var \Magento\FunctionalTestingFramework\Test\Config\Dom $configMerger */
+        $configMerger = null;
+        foreach ($fileList as $key => $content) {
+            try {
+                if (!$configMerger) {
+                    $configMerger = $this->createConfigMerger(
+                        $this->domDocumentClass,
+                        $content,
+                        $fileList->getFilename(),
+                        $exceptionCollector
+                    );
+                } else {
+                    $configMerger->merge($content, $fileList->getFilename(), $exceptionCollector);
+                }
+            } catch (\Magento\FunctionalTestingFramework\Config\Dom\ValidationException $e) {
+                throw new \Exception("Invalid XML in file " . $key . ":\n" . $e->getMessage());
+            }
+        }
+        $exceptionCollector->throwException();
+        if ($this->validationState->isValidationRequired()) {
+            if ($configMerger && !$configMerger->validate($this->schemaFile, $errors)) {
+                $message = "Invalid Document \n";
+                throw new \Exception($message . implode("\n", $errors));
+            }
+        }
+
+        $output = [];
+        if ($configMerger) {
+            $output = $this->converter->convert($configMerger->getDom());
+        }
+        return $output;
+    }
+
+    /**
+     * Return newly created instance of a config merger
+     *
+     * @param string $mergerClass
+     * @param string $initialContents
+     * @param string $filename
+     * @param ExceptionCollector $exceptionCollector
+     * @return \Magento\FunctionalTestingFramework\Config\Dom
+     * @throws \UnexpectedValueException
+     */
+    protected function createConfigMerger($mergerClass, $initialContents, $filename = null, $exceptionCollector = null)
+    {
+        $result = new $mergerClass(
+            $initialContents,
+            $filename,
+            $exceptionCollector,
+            $this->idAttributes,
+            null,
+            $this->perFileSchema
+        );
+        if (!$result instanceof \Magento\FunctionalTestingFramework\Config\Dom) {
+            throw new \UnexpectedValueException(
+                "Instance of the DOM config merger is expected, got {$mergerClass} instead."
+            );
+        }
+        return $result;
+    }
+}
