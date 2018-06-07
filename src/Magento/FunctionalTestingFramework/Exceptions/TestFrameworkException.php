5700373b931c2cdb0ed0998ca0a4df41b7708174
KevinBKozan <kkozan@magento.com>
2018-06-07 10:16:58 -0500

    MQE-1043: MFTF force flag should ignore the magento base url even when valid
    - ModuleResolver now completely ignores trying to get enabledModules if --force is set
    - Shuffled around exception throwing for faster failure
    - Fix for MQE-1017 included, exceptions are now more verbose and precise as to when they are thrown.

--- src/Magento/FunctionalTestingFramework/Exceptions/TestFrameworkException.php
+++ src/Magento/FunctionalTestingFramework/Exceptions/TestFrameworkException.php
@@ -6,6 +6,8 @@
 
 namespace Magento\FunctionalTestingFramework\Exceptions;
 
+use Magento\FunctionalTestingFramework\Util\Logger\LoggingUtil;
+
 /**
  * Class TestFrameworkException
  */
@@ -14,9 +16,17 @@ class TestFrameworkException extends \Exception
     /**
      * TestFrameworkException constructor.
      * @param string $message
+     * @param array $context
+     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
      */
-    public function __construct($message)
+    public function __construct($message, $context = [])
     {
+        list($childClass, $callingClass) = debug_backtrace(false, 2);
+        LoggingUtil::getInstance()->getLogger($callingClass['class'])->error(
+            $message,
+            $context
+        );
+
         parent::__construct($message);
     }
 }
