2cfa59a04bb12d267c4afd912e670f1aa1408a37
Alex Calandra <acalandra@magento.com>
2018-06-27 09:53:40 -0500

    MQE-1068: Require Issue ID for Skipped Test
    - Removed verbose requirement for deprecation warnign flag

--- src/Magento/FunctionalTestingFramework/Test/Util/AnnotationExtractor.php
+++ src/Magento/FunctionalTestingFramework/Test/Util/AnnotationExtractor.php
@@ -75,12 +75,10 @@ class AnnotationExtractor extends BaseObjectExtractor
             }
             // TODO deprecation|deprecate MFTF 3.0.0
             if ($annotationKey == "group" && in_array("skip", $annotationValues)) {
-                if (MftfApplicationConfig::getConfig()->verboseEnabled()) {
-                    LoggingUtil::getInstance()->getLogger(AnnotationExtractor::class)->warning(
-                        "Use of group skip will be deprecated in MFTF 3.0.0. Please update tests to use skip tags.",
-                        ["test" => $filename]
-                    );
-                }
+                LoggingUtil::getInstance()->getLogger(AnnotationExtractor::class)->warning(
+                    "Use of group skip will be deprecated in MFTF 3.0.0. Please update tests to use skip tags.",
+                    ["test" => $filename]
+                );
             }
 
             $annotationObjects[$annotationKey] = $annotationValues;
