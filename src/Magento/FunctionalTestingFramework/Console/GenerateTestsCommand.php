d97b01352a1fda16f59f1a676fa815217ae3f1b3
Kevin Kozan <kkozan@adobe.com>
2019-09-13 14:39:46 -0500

    MQE-1582: Enable Testers To Run Skipped Tests
    - Fix tag name to allow-skipped as per AC

--- src/Magento/FunctionalTestingFramework/Console/GenerateTestsCommand.php
+++ src/Magento/FunctionalTestingFramework/Console/GenerateTestsCommand.php
@@ -71,7 +71,7 @@ class GenerateTestsCommand extends BaseGenerateCommand
         $debug = $input->getOption('debug') ?? MftfApplicationConfig::LEVEL_DEVELOPER; // for backward compatibility
         $remove = $input->getOption('remove');
         $verbose = $output->isVerbose();
-        $allowSkipped = $input->getOption('allowSkipped');
+        $allowSkipped = $input->getOption('allow-skipped');
 
         // Set application configuration so we can references the user options in our framework
         MftfApplicationConfig::create(
