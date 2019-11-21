f039692292ce643ae9382575c8757912f0596e1b
Soumya Unnikrishnan <sunnikri@adobe.com>
2019-11-21 09:09:25 -0600

    MQE-1774: Review community PR #343 Fix for pipeline step failures on execution of bin/magento from WebDriver.

--- src/Magento/FunctionalTestingFramework/Module/MagentoWebDriver.php
+++ src/Magento/FunctionalTestingFramework/Module/MagentoWebDriver.php
@@ -517,11 +517,13 @@ class MagentoWebDriver extends WebDriver
      */
     public function magentoCLI($command, $arguments = null)
     {
-        try {
-            return $this->shellExecMagentoCLI($command, $arguments);
-        } catch (\Exception $exception) {
-            return $this->curlExecMagentoCLI($command, $arguments);
-        }
+        return $this->curlExecMagentoCLI($command, $arguments);
+        //TODO: calling bin/magento from pipeline is timing out, needs investigation (ref: MQE-1774)
+//        try {
+//            return $this->shellExecMagentoCLI($command, $arguments);
+//        } catch (\Exception $exception) {
+//            return $this->curlExecMagentoCLI($command, $arguments);
+//        }
     }
 
     /**
