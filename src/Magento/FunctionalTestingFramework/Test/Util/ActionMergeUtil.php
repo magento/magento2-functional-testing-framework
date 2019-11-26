9b42d34814ebdbde4d73b6383cfa1c90b19eacf6
Tom Reece <treece@adobe.com>
2019-11-26 11:22:03 -0600

    MQE-1374: waitForPageLoads being inserted unnecessarily

--- src/Magento/FunctionalTestingFramework/Test/Util/ActionMergeUtil.php
+++ src/Magento/FunctionalTestingFramework/Test/Util/ActionMergeUtil.php
@@ -217,8 +217,32 @@ class ActionMergeUtil
      */
     private function insertWaits()
     {
+        # This list is sorted alphabetically. Please maintain that order when modifying.
+        $blacklist = [
+            "assertArrayHasKey", "assertArrayIsSorted", "assertArrayNotHasKey", "assertArraySubset", "assertContains",
+            "assertCount", "assertElementContainsAttribute", "assertEmpty", "assertEquals", "assertFalse",
+            "assertFileExists", "assertFileNotExists", "assertGreaterOrEquals", "assertGreaterThan",
+            "assertGreaterThanOrEqual", "assertInstanceOf", "assertInternalType", "assertIsEmpty", "assertLessOrEquals",
+            "assertLessThan", "assertLessThanOrEqual", "assertNotContains", "assertNotEmpty", "assertNotEquals",
+            "assertNotInstanceOf", "assertNotNull", "assertNotRegExp", "assertNotSame", "assertNull", "assertRegExp",
+            "assertSame", "assertStringStartsNotWith", "assertStringStartsWith", "assertTrue", "createData",
+            "deleteData", "dontSee", "dontSeeCheckboxIsChecked", "dontSeeCookie", "dontSeeCurrentUrlEquals",
+            "dontSeeCurrentUrlMatches", "dontSeeElement", "dontSeeElementInDOM", "dontSeeInCurrentUrl",
+            "dontSeeInField", "dontSeeInFormFields", "dontSeeInPageSource", "dontSeeInSource", "dontSeeInTitle",
+            "dontSeeJsError", "dontSeeLink", "dontSeeOptionIsSelected", "expectException", "formatMoney",
+            "generateDate", "getData", "grabAttributeFrom", "grabCookie", "grabFromCurrentUrl", "grabMultiple",
+            "grabPageSource", "grabTextFrom", "grabValueFrom", "magentoCLI", "parseFloat", "see",
+            "seeCheckboxIsChecked", "seeCookie", "seeCurrentUrlEquals", "seeCurrentUrlMatches", "seeElement",
+            "seeElementInDOM", "seeInCurrentUrl", "seeInField", "seeInFormFields", "seeInPageSource", "seeInPopup",
+            "seeInSource", "seeInTitle", "seeLink", "seeNumberOfElements", "seeOptionIsSelected", "switchToIFrame",
+            "switchToNextTab", "switchToPreviousTab", "switchToWindow", "updateData", "wait", "waitForAjaxLoad",
+            "waitForElement", "waitForElementChange", "waitForElementNotVisible", "waitForElementVisible", "waitForJS",
+            "waitForLoadingMaskToDisappear", "waitForPageLoad", "waitForPwaElementNotVisible",
+            "waitForPwaElementVisible", "waitForText"
+        ];
+
         foreach ($this->orderedSteps as $step) {
-            if ($step->getTimeout()) {
+            if ($step->getTimeout() && !in_array($step->getType(), $blacklist)) {
                 $waitStepAttributes = [self::WAIT_ATTR => $step->getTimeout()];
                 $waitStep = new ActionObject(
                     $step->getStepKey() . self::WAIT_ACTION_SUFFIX,
