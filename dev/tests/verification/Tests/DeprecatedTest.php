9775b5eec21d75f791531bf3f1baabddceda1406
Alex Kolesnyk <kolesnyk@adobe.com>
2020-01-27 12:59:24 -0600

    MFTF deprecation notice attributes
     - add verification test

---
+++ dev/tests/verification/Tests/DeprecatedTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace tests\verification\Tests;
+
+use tests\util\MftfTestCase;
+
+class DeprecatedTest extends MftfTestCase
+{
+    /**
+     * Tests flat generation of a deprecated test which uses deprecated entities.
+     *
+     * @throws \Exception
+     * @throws \Magento\FunctionalTestingFramework\Exceptions\TestReferenceException
+     */
+    public function testDeprecatedTestEntitiesGeneration()
+    {
+        $this->generateAndCompareTest('DeprecatedTest');
+    }
+}
