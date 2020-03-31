fbcbe1a48abce101b6ec1e69f64bed74fd8ab353
Tom Reece <treece@adobe.com>
2020-03-31 17:02:16 -0500

    MQE-2042: Port MFTF Coverage to Jenkins
    - Add copyright

--- etc/codecoverage/test.php
+++ etc/codecoverage/test.php
@@ -1,4 +1,10 @@
 <?php
+
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
 $test = $_GET['test'] ?? "NO_TEST_SPECIFIED";
 file_put_contents('CURRENT_TEST', $test);
 echo 'SET CURRENT TEST TO ' . $test;
