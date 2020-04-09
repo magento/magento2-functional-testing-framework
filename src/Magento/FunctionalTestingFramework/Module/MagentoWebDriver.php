e96f14de4731c99a622a2a481fa618cdab95d5fd
Soumya Unnikrishnan <sunnikri@adobe.com>
2020-04-09 16:05:40 -0500

    MQE-2078: [PHPUnit 9] assertContains and assertNotContains breaking changes

--- src/Magento/FunctionalTestingFramework/Module/MagentoWebDriver.php
+++ src/Magento/FunctionalTestingFramework/Module/MagentoWebDriver.php
@@ -256,7 +256,7 @@ class MagentoWebDriver extends WebDriver
         $actualUrl = $this->webDriver->getCurrentURL();
         $comparison = "Expected: $needle\nActual: $actualUrl";
         AllureHelper::addAttachmentToCurrentStep($comparison, 'Comparison');
-        $this->assertStringContainsString($needle, $actualUrl);
+        $this->assertStringNotContainsString($needle, $actualUrl);
     }
 
     /**
