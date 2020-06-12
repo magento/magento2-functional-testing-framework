7a7e4033cab73d19d1b5ec05a93cd9541bc4f6e8
soumyau <sunnikri@adobe.com>
2020-06-12 14:19:58 -0500

    MQE-2140: [chrome81] dragAndDrop with x and y offsets no longer works (#725)
    * MQE-2143: [chrome81] dragAndDrop with x and y offsets no longer works
    
    * MQE-2140: [chrome81] dragAndDrop with x and y offsets no longer works
    
    * MQE-2140: [chrome81] dragAndDrop with x and y offsets no longer works
    
    * MQE-2140: [chrome81] dragAndDrop with x and y offsets no longer works

--- src/Magento/FunctionalTestingFramework/Module/MagentoWebDriver.php
+++ src/Magento/FunctionalTestingFramework/Module/MagentoWebDriver.php
@@ -745,23 +745,24 @@ class MagentoWebDriver extends WebDriver
      */
     public function dragAndDrop($source, $target, $xOffset = null, $yOffset = null)
     {
-        if ($xOffset !== null || $yOffset !== null) {
-            $snodes = $this->matchFirstOrFail($this->baseElement, $source);
-            $tnodes = $this->matchFirstOrFail($this->baseElement, $target);
+        $snodes = $this->matchFirstOrFail($this->baseElement, $source);
+        $tnodes = $this->matchFirstOrFail($this->baseElement, $target);
+        $action = new WebDriverActions($this->webDriver);
 
+        if ($xOffset !== null || $yOffset !== null) {
             $targetX = intval($tnodes->getLocation()->getX() + $xOffset);
             $targetY = intval($tnodes->getLocation()->getY() + $yOffset);
 
             $travelX = intval($targetX - $snodes->getLocation()->getX());
             $travelY = intval($targetY - $snodes->getLocation()->getY());
-
-            $action = new WebDriverActions($this->webDriver);
-            $action->moveToElement($snodes)->perform();
-            $action->clickAndHold($snodes)->perform();
-            $action->moveByOffset($travelX, $travelY)->perform();
+            $action->moveToElement($snodes);
+            $action->clickAndHold($snodes);
+            $action->moveByOffset($travelX, $travelY);
             $action->release()->perform();
         } else {
-            parent::dragAndDrop($source, $target);
+            $action->clickAndHold($snodes);
+            $action->moveToElement($tnodes);
+            $action->release($tnodes)->perform();
         }
     }
 
