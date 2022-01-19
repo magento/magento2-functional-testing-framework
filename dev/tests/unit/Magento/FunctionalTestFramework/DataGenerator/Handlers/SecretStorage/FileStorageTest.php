e67dd090366c89d02df5e1145eacc8cd82ec7158
glo00108 <glo00108@adobe.com>
2022-01-19 18:29:23 +0530

    add exception assert

--- dev/tests/unit/Magento/FunctionalTestFramework/DataGenerator/Handlers/SecretStorage/FileStorageTest.php
+++ dev/tests/unit/Magento/FunctionalTestFramework/DataGenerator/Handlers/SecretStorage/FileStorageTest.php
@@ -8,19 +8,22 @@ declare(strict_types=1);
 namespace tests\unit\Magento\FunctionalTestFramework\DataGenerator\Handlers\SecretStorage;
 
 use Magento\FunctionalTestingFramework\DataGenerator\Handlers\SecretStorage\FileStorage;
+use Magento\FunctionalTestingFramework\Exceptions\TestFrameworkException;
 use ReflectionClass;
+use ReflectionException;
 use tests\unit\Util\MagentoTestCase;
 
 class FileStorageTest extends MagentoTestCase
 {
     /**
      * Test basic encryption/decryption functionality in FileStorage class.
+     * @throws TestFrameworkException|ReflectionException
      */
     public function testBasicEncryptDecrypt(): void
     {
         $testKey = 'magento/myKey';
         $testValue = 'myValue';
-        $creds = ["$testKey=$testValue"];
+        $cred = ["$testKey=$testValue"];
 
         $fileStorage = new FileStorage();
         $reflection = new ReflectionClass(FileStorage::class);
@@ -28,7 +31,7 @@ class FileStorageTest extends MagentoTestCase
         // Emulate initialize() function result with the test credentials
         $reflectionMethod = $reflection->getMethod('encryptCredFileContents');
         $reflectionMethod->setAccessible(true);
-        $secretData = $reflectionMethod->invokeArgs($fileStorage, [$creds]);
+        $secretData = $reflectionMethod->invokeArgs($fileStorage, [$cred]);
 
         // Set encrypted test credentials to the private 'secretData' property
         $reflectionProperty = $reflection->getProperty('secretData');
@@ -48,34 +51,29 @@ class FileStorageTest extends MagentoTestCase
 
     /**
      * Test empty value encryption/decryption functionality in FileStorage class.
+     * @return void
+     * @throws TestFrameworkException|ReflectionException
      */
     public function testEmptyValueEncryptDecrypt(): void
     {
-      $testKey = 'magento/myKey';
-      $testValue = '';
-      $creds = ["$testKey"];
+        $this->expectException(TestFrameworkException::class);
 
-      $fileStorage = new FileStorage();
-      $reflection = new ReflectionClass(FileStorage::class);
+        $testKey = 'magento/myKey';
+        $cred = ["$testKey"];
 
-      // Emulate initialize() function result with the test credentials
-      $reflectionMethod = $reflection->getMethod('encryptCredFileContents');
-      $reflectionMethod->setAccessible(true);
-      $secretData = $reflectionMethod->invokeArgs($fileStorage, [$creds]);
+        $fileStorage = new FileStorage();
+        $reflection = new ReflectionClass(FileStorage::class);
 
-      // Set encrypted test credentials to the private 'secretData' property
-      $reflectionProperty = $reflection->getProperty('secretData');
-      $reflectionProperty->setAccessible(true);
-      $reflectionProperty->setValue($fileStorage, $secretData);
+        // Emulate initialize() function result with the test credentials
+        $reflectionMethod = $reflection->getMethod('encryptCredFileContents');
+        $reflectionMethod->setAccessible(true);
+        $secretData = $reflectionMethod->invokeArgs($fileStorage, [$cred]);
 
-      $encryptedCred = $fileStorage->getEncryptedValue($testKey);
+        // Set encrypted test credentials to the private 'secretData' property
+        $reflectionProperty = $reflection->getProperty('secretData');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($fileStorage, $secretData);
 
-      // assert the value we've gotten is in fact not identical to our test value
-      $this->assertNotEquals($testValue, $encryptedCred);
-
-      $actualValue = $fileStorage->getDecryptedValue($encryptedCred);
-
-      // assert that we are able to successfully decrypt our secret value
-      $this->assertEquals($testValue, $actualValue);
+        $fileStorage->getEncryptedValue($testKey);
     }
 }
