cfec610e9a63b04ede13323b5c67e0af8351457e
Ji Lu <jilu1@adobe.com>
2023-12-06 14:21:35 -0600

    ACQE-5871: Add ability to increase default MagentoCLI timeout

--- src/Magento/FunctionalTestingFramework/Test/Objects/ActionObject.php
+++ src/Magento/FunctionalTestingFramework/Test/Objects/ActionObject.php
@@ -192,7 +192,7 @@ class ActionObject
     public static function getDefaultMagentoCLIWaitTimeout()
     {
         $timeout = getenv('MAGENTO_CLI_WAIT_TIMEOUT');
-        return $timeout !== false ? $timeout : self::DEFAULT_COMMAND_WAIT_TIMEOUT;
+        return !empty($timeout) ? $timeout : self::DEFAULT_COMMAND_WAIT_TIMEOUT;
     }
 
     /**
