fc97172c75df3d55c19b030f055c94039cfa1fb1
manjusha729 <93243302+manjusha729@users.noreply.github.com>
2025-01-06 11:34:37 +0530

    Update msq.php

--- src/Magento/FunctionalTestingFramework/Util/msq.php
+++ src/Magento/FunctionalTestingFramework/Util/msq.php
@@ -1,34 +1,28 @@
 <?php
-
-declare(strict_types=1);
-
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
+ 
 use Magento\FunctionalTestingFramework\Module\MagentoSequence;
 
 if (!function_exists('msq')) {
     /**
      * Return unique sequence within test.
      *
-     * @param int|string|null $id Optional identifier for the sequence.
-     * @return string The generated unique sequence.
+     * @param null $id
+     * @return string
      */
-    function msq(int|string|null $id = null): string
+    function msq(int|string|null $id = null)
     {
-        if ($id !== null && isset(MagentoSequence::$hash[$id])) {
+        if ($id and isset(MagentoSequence::$hash[$id])) {
             return MagentoSequence::$hash[$id];
         }
-
-        $prefix = MagentoSequence::$prefix ?? '';
-        $sequence = $prefix . uniqid('', true); // Use true for high-entropy ID
-
-        if ($id !== null) {
-            MagentoSequence::$hash[$id] = $sequence; // Avoid dynamic properties
+        $prefix = MagentoSequence::$prefix;
+        $sequence = $prefix . uniqid();
+        if ($id) {
+            MagentoSequence::$hash[$id] = $sequence;
         }
-
         return $sequence;
     }
 }
@@ -37,22 +31,19 @@ if (!function_exists('msqs')) {
     /**
      * Return unique sequence within suite.
      *
-     * @param int|string|null $id Optional identifier for the suite sequence.
-     * @return string The generated unique suite sequence.
+     * @param null $id
+     * @return string
      */
-    function msqs(int|string|null $id = null): string
+    function msqs(int|string|null $id = null)
     {
-        if ($id !== null && isset(MagentoSequence::$suiteHash[$id])) {
+        if ($id and isset(MagentoSequence::$suiteHash[$id])) {
             return MagentoSequence::$suiteHash[$id];
         }
-
-        $prefix = MagentoSequence::$prefix ?? '';
-        $sequence = $prefix . uniqid('', true); // Use true for high-entropy ID
-
-        if ($id !== null) {
-            MagentoSequence::$suiteHash[$id] = $sequence; // Avoid dynamic properties
+        $prefix = MagentoSequence::$prefix;
+        $sequence = $prefix . uniqid();
+        if ($id) {
+            MagentoSequence::$suiteHash[$id] = $sequence;
         }
-
         return $sequence;
     }
 }
