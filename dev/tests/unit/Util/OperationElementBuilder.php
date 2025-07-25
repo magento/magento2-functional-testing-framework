7e6c07cb481e5d79bbafbd9425c7f77e6a56bc44
chandrakalahanchinal <del66267@adobe.com>
2025-07-25 13:50:38 +0530

    [ACQE-7959] Fix MFTF Copyright Check
    - Update copyright in dev folder files.

--- dev/tests/unit/Util/OperationElementBuilder.php
+++ dev/tests/unit/Util/OperationElementBuilder.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2017 Adobe
+ * All Rights Reserved.
  */
+
 namespace tests\unit\Util;
 
 use Magento\FunctionalTestingFramework\DataGenerator\Handlers\OperationDefinitionObjectHandler;
--- dev/tests/verification/Tests/HookActionsTest.php
+++ dev/tests/verification/Tests/HookActionsTest.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2018 Adobe
+ * All Rights Reserved.
  */
+
 namespace tests\verification\Tests;
 
 use tests\util\MftfTestCase;
